e09af9a174dd294906dd25d96139a3d33c5b8672
Mercury13 <Mercury13@users.noreply.github.com>
2026-04-21 23:21:18 +0300

    Unescape ini translations before applying
    Add _unescapeSlashes to decode escaped backslashes and \n in ini translation strings with sanity checks (error on unknown escapes or trailing backslash, cap at 30 replacements). Introduce _hackTranslation to run the unescape step on incoming translations and update retranslate() to use the renamed id variable and call the hack before applying translations.

--- Lang-src/!Scripts/xet.py
+++ Lang-src/!Scripts/xet.py
@@ -25,6 +25,33 @@ def fixupSettings(root : ET.Element, outLang : string):
         raise Exception('Cannot find <info/transl> in UTran for some reason')
     hTransl.attrib['lang'] = outLang
 
+def _unescapeSlashes(x : string, id : string) -> string:
+    pos = 0
+    iReplacement = 0
+    xOld = x
+    while True:
+        pSlash = x.find('\\', pos)
+        # not found / last
+        if pSlash < 0:
+            return x
+        if pSlash + 1 >= len(x):
+            raise Exception(f'Strange ending escape in <{id}>')
+        nextc = x[pSlash + 1]
+        if nextc == '\\':
+            x = x[:pSlash] + '\\' + x[(pSlash + 2):]
+            pos = pSlash + 2
+        elif nextc == 'n':
+            x = x[:pSlash] + '\n' + x[(pSlash + 2):]
+            pos = pSlash + 2
+        else:
+            raise Exception(f'Strange escape \\{nextc} in <{id}>')
+        iReplacement = iReplacement + 1
+        if iReplacement >= 30:
+            raise Exception(f'Too many replacements in <{id}>, before <{xOld}>, after <{x}>')
+
+def _hackTranslation(hString : ET.Element, iniTransl : string, id : string):
+    realTransl = _unescapeSlashes(iniTransl, id)
+
 def _stickIds(prefix : string, suffix : string):
     if prefix == '':
         return suffix
@@ -49,10 +76,11 @@ def _retranslateRec(node : ET.Element, ini : xini.Ini, prefix : string):
         newId = v.attrib['id']
         if newId == '':
             raise Exception(f'Empty id in UTransl / {prefix} / string #{index}')
-        newPrefix = _stickIds(prefix, newId)
-        translation = ini.at(newPrefix)
+        newBigId = _stickIds(prefix, newId)
+        translation = ini.at(newBigId)
         if translation is None:
             raise Exception(f'Cannot translate {newPrefix}')
+        _hackTranslation(v, translation, newBigId)
 
 def retranslate(root : ET.Element, ini : xini.Ini):
     hFile = root.find('file')
